4d4e077bdcde4b6c8f558c2bee731b5ef5b45fc7
Shah Wali <69451223+shahwalidjango@users.noreply.github.com>
2024-10-31 13:16:50 +0500

    Merge remote-tracking branch 'origin/main'

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,24 +1,5 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-application = get_wsgi_application()
-app = application
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_blog.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 application = get_wsgi_application()
